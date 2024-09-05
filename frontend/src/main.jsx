8640850a78e46007170bb8248ed5fede23776dd9
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2024-09-05 07:49:05 +0530

    New Update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx'; // Ensure you're using default import
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
